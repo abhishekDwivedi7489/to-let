05ce4389b82baa8355dcabef2ffd8dff2ead77ea
abhishekDwivedi7489 <143690669+abhishekDwivedi7489@users.noreply.github.com>
2024-02-22 17:17:56 +0530

    Update FacilityForm.jsx
    change facility form and add a button

--- src/component/Dashboard/AddRooms/homebuild/FacilityForm.jsx
+++ src/component/Dashboard/AddRooms/homebuild/FacilityForm.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
+
 import { useSelector } from 'react-redux'
 import {RxCross2} from 'react-icons/rx'
 
 const FacilityForm = ({labelName,name,setValue,getValues,register,errors,placeholder,tags,setTags}) => {
     const {home, editHome} = useSelector((state)=>state.home)
-      
+         const [chipValue, setChipvalue] = useState("")
     
         //setValue(tags);
         useEffect(()=>{
@@ -20,60 +21,67 @@ const FacilityForm = ({labelName,name,setValue,getValues,register,errors,placeho
             setValue(name,tags);
         },[tags])
      
-        const handleKeyDown = (event) =>{
-          
-              if(event.key === " " || event.key === "," ){
+        const handleAddTags = (event) =>{
                 event.preventDefault();
-                const chipValue = event.target.value.trim();
+                event.stopPropagation();
                 setTags([...tags, chipValue])
-                event.target.value=" "
+                setChipvalue("")
               }
     
-        }
-         //console.log("tags",tags)
+        
+      
         const closeHandler=(index)=>{
              // setTags(tags.filter((ele, i) => i !== index))
               const updateList = [...tags];
               updateList.splice(index,1);
               setTags(updateList);
         }
-    
-      return (
-        <div className=''>
-           
-            <div className='flex flex-row gap-x-3 w-full'>
-            { 
-                
-                tags?.map( (tag, index) => (
-                    <div key={index} className='bg-yellow-900 max-w-max flex flex-row items-center space-x-1 max-h-max rounded-full py-1 px-3'>
-                        <span className='text-white'>{tag}</span>
-                        <span onClick={()=>closeHandler(index)}>
-                        <RxCross2  className='text-richblack-100 text-[12px] font-bold'/>
-                        </span>
-                    </div>
-                ))
-            }
+
+        return (
+          <div className=''>
+             
+              <div className='flex flex-row gap-x-3 w-full'>
+              { 
+                  
+                  tags?.map( (tag, index) => (
+                      <div key={index} className='bg-yellow-900 max-w-max flex flex-row items-center space-x-1 max-h-max rounded-full py-1 px-3'>
+                          <span className='text-white'>{tag}</span>
+                          <span onClick={()=>closeHandler(index)}>
+                          <RxCross2  className='text-richblack-100 text-[12px] font-bold'/>
+                          </span>
+                      </div>
+                  ))
+              }
+              </div>
+      
+            <div className="flex flex-col items-start space-y-2">
+            <label htmlFor={name} className="text-sm text-richblack-5">{labelName}<sup className='text-pink-200'></sup></label>
+             
+             <input
+              type='text'
+              id={name}
+              value={chipValue}
+              placeholder={placeholder}
+              className='w-full form-style'
+              onChange={(e) => setChipvalue(e.target.value)}
+             />
+              <button
+              onClick={handleAddTags}
+              className='font-semibold text-yellow-50'
+             >
+              Add
+             </button>
+             {
+              errors[name] && (
+                  <span className="ml-2 text-xs tracking-wide text-pink-200">Facilities are require And Press Enter</span>
+              )
+             }
             </div>
-    
-          <div className="flex flex-col items-start space-y-2">
-          <label htmlFor={name} className="text-sm text-richblack-5">{labelName}<sup className='text-pink-200'></sup></label>
-           
-           <input
-            type='text'
-            id={name}
-            placeholder={placeholder}
-            className='w-full form-style'
-            onKeyUp={handleKeyDown}
-           />
-           {
-            errors[name] && (
-                <span className="ml-2 text-xs tracking-wide text-pink-200">Facilities are require And Press Enter</span>
-            )
-           }
+      
           </div>
+        )
     
-        </div>
-      )
+      
     }
     
 
