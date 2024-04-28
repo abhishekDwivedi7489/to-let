879036bac792e7112d266bcaea3108ffa9efd4c7
abhishekDwivedi7489 <143690669+abhishekDwivedi7489@users.noreply.github.com>
2024-04-28 17:47:34 +0530

    Update homeSlice.js

--- src/slices/homeSlice.js
+++ src/slices/homeSlice.js
@@ -4,17 +4,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     home : null,
     room : null,
-    categoryRoom : null
+   
 }
 
 const homeSlice = createSlice({
     name : "Room",
     initialState,
     reducers:{
-        setCategoryRoom(state, action){
-            state.categoryRoom = action.payload
-        },
-
         setHome(state, action){
             state.home = action.payload
         },
