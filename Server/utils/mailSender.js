529e37c5ed26258d6299670f0361a7a3c59934ff
abhishekDwivedi7489 <143690669+abhishekDwivedi7489@users.noreply.github.com>
2024-04-28 15:40:30 +0530

    Update mailSender.js
    UPDATE EMAIL TITLE

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -13,7 +13,7 @@ const mailSender = async(email, title, body)=>{
         });
 
         let info = await transporter.sendMail({
-            from:"TO-LET",
+            from:`TO-LET | ROOM BOOK <${process.env.MAIL_USER}>`,
             to:`${email}`,
             subject:`${title}`,
             html:`${body}`
